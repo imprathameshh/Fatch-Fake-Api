e81235d4540c8f2657819826df37d81c64cdccb5
prathmesh4 <prathmesh@krishaweb.com>
2023-04-26 00:13:44 +0530

    redirect to another page

--- assgnment2.js
+++ assgnment2.js
@@ -9,12 +9,12 @@ let api = fetch("https://fakestoreapi.com/products")
       output += `
                 <li> 
                 <div class="product">
-                <a href="moreInfo.html&${giphy.id}">
+                <a href="moreInfo.html?${giphy.id}">
                 <img src="${giphy.image}">
                 <h2 class="title">${giphy.title}</h2>
                 <p class= "price">
                 <span>${giphy.price}</span>
-                <span> \u20B9</span>   
+                <span> \u20B9</span>  
                 </p>
                 </a>
                 </div>
